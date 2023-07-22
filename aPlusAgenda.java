991f18456ded4fc904e80fb9325bde1ac9394048
Alex You <alex.you0825@gmail.com>
2023-07-22 18:07:06 -0400

    extended JFrame

--- aPlusAgenda.java
+++ aPlusAgenda.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.time.*;
 import java.util.HashMap;
 
-public class aPlusAgenda{
+public class aPlusAgenda extends JFrame {
 	final int W = 1920; // width
 	final int H = 1080; // height
 
@@ -17,7 +17,7 @@ public class aPlusAgenda{
 	final int HourHeight = (CalendarBottom - CalendarTop) / NumHours;
 	final int DayWidth = (W - SidePadding*2) / 7;
 
-	JFrame f;
+	//JFrame f;
 
 	//HashMap<LocalDate, ArrayList<Event>> events = new HashMap<LocalDate, ArrayList<Event>>();
 
@@ -36,39 +36,38 @@ public class aPlusAgenda{
     }
 
     void setup() {
-		f = new JFrame ("A+ Agenda");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setLayout(new BorderLayout());
-		f.getContentPane().setBackground(new Color(223, 231, 255));
-		f.setSize(W, H);
-		f.setLocationRelativeTo(null);
-		f.setVisible(true);
-
+		//f = new JFrame ("A+ Agenda");
+		this.setTitle("A+ Agenda");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.getContentPane().setBackground(new Color(223, 231, 255));
+		this.setSize(W, H);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
 		//JPanel backPanel = new JPanel();
 		//backPanel.setBackground(new Color(182, 190, 216));
 		//backPanel.setBounds(0,0,W,500);
 		//backPanel.setPreferredSize(new Dimension(100, 100));
 		//f.add(backPanel, BorderLayout.NORTH);
 
-		JPanel linePanel = new JPanel() {
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-
-				g.setColor(new Color(182, 190, 216));
-				((Graphics2D) g).setStroke(new BasicStroke(2));
-
-				weeklyView(g);
-			}
-		};
-
-		f.add(linePanel, BorderLayout.CENTER);
-
-
     	// f.setColor(Color(182, 190, 216));
 		// g.fillRect(0,0,W,100);
 
-    }
+    
+	JPanel linePanel = new JPanel() {
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+
+			g.setColor(new Color(182, 190, 216));
+			((Graphics2D) g).setStroke(new BasicStroke(2));
+
+			weeklyView(g);
+		}
+	};
+		this.add(linePanel, BorderLayout.CENTER);
+	}
+	
 
     void weeklyView(Graphics g) {
 		drawLines(g);
