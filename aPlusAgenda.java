f4a7cf5a1d13dd17b40a7293da4f52338301b1e9
EmChen06 <emileechen95@gmail.com>
2023-07-22 16:02:02 -0400

    Graphics and Hashmap

--- aPlusAgenda.java
+++ aPlusAgenda.java
@@ -1,17 +1,21 @@
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.time.*;
+import java.util.HashMap;
 
-public class APlusAgenda extends JFrame {
+public class APlusAgenda{
 	final int W = 1920;//width
 	final int H = 1080;//height
 	JFrame f;
 
-    // Color lineColor = new Color(182, 190, 216, 104);
-    // Color textColor = new Color(23, 44, 112);
-    // Font typeFont = new Font("Montserrat", Font.BOLD, 24);
-    // Font monthFont = new Font("Montserrat", Font.PLAIN, 56);
-    // Font timeFont = new Font("Montserrat", Font.PLAIN, 20);
+	HashMap<LocalDate, ArrayList<Event>> events = new HashMap<LocalDate, ArrayList<Event>>();
+
+    // Color lineColor = Color(182, 190, 216, 104);
+    // Color textColor = Color(23, 44, 112);
+    // Font typeFont = Font("Montserrat", Font.BOLD, 24);
+    // Font monthFont = Font("Montserrat", Font.PLAIN, 56);
+    // Font timeFont = Font("Montserrat", Font.PLAIN, 20);
 	
     public static void main(String[] args) {
 		new APlusAgenda();
@@ -23,21 +27,60 @@ public class APlusAgenda extends JFrame {
     }
     
     void setup() {
-		f = new JFrame("A+ Agenda");
+		f = new JFrame ("A+ Agenda");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLayout(new BorderLayout());
+		f.getContentPane().setBackground(new Color(223, 231, 255));
 		f.setSize(W,H);
-		frame.setLayout(new GridBagLayout());
 		f.setLocationRelativeTo(null);
 		f.setVisible(true);
 
-    	// gc.setBackgroundColor(new Color(223, 231, 255));
-    	// gc.setColor(new Color(182, 190, 216));
-    	// gc.fillRect(0,0,W,100);
+		JPanel backPanel = new JPanel();
+		backPanel.setBackground(new Color(182, 190, 216));
+		backPanel.setBounds(0,0,W,500);
+		backPanel.setPreferredSize(new Dimension(100, 100));
+
+		f.add(backPanel, BorderLayout.NORTH);
+
+		JPanel linePanel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+
+				g.setColor(new Color(182, 190, 216));
+				((Graphics2D) g).setStroke(new BasicStroke(2));
+
+				g.drawLine(50, 150, W, 150);
+				g.drawLine(50, 250, W, 250); 
+				g.drawLine(50, 350, W, 350); 
+				g.drawLine(50, 450, W, 450); 
+				g.drawLine(50, 550, W, 550); 
+				g.drawLine(50, 650, W, 650); 
+				g.drawLine(50, 750, W, 750); 
+				g.drawLine(50, 850, W, 850); 
+				g.drawLine(50, 950, W, 950); 
+				g.drawLine(50, 1050, W, 1050);
+				g.drawLine(50, 100, 50, H);
+				g.drawLine(320, 100, 320, H);
+				g.drawLine(590, 100, 590, H);
+				g.drawLine(860, 100, 860, H);
+				g.drawLine(1130, 100, 1130, H);
+				g.drawLine(1400, 100, 1400, H);
+				g.drawLine(1670, 100, 1670, H);
+
+			}
+		};
+
+		f.add(linePanel, BorderLayout.CENTER);
+
+		
+    	// f.setColor(Color(182, 190, 216));
+		// gc.fillRect(0,0,W,100);
 
     }
     
     void weeklyView() {
 
-    	
     	// //times
     	// gc.drawLine(50, 150, W, 150);
     	// gc.drawLine(50, 250, W, 250); 
