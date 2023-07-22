5e108390713bf328bd4554287bc2d5dd7ba4d111
Garklein <garklein97@gmail.com>
2023-07-22 08:30:25 -0400

    hsa2

--- aPlusAgenda.java
+++ aPlusAgenda.java
@@ -1,7 +1,14 @@
 import java.util.*;
+import hsa2.GraphicsConsole;
 
-public class aPlusAgenda {
+public class APlusAgenda {
+    final int W = 500; // width
+    GraphicsConsole gc = new GraphicsConsole(W, W, "testing");
     public static void main(String[] args) {
-        //i havent coded in so long :D
+        new APlusAgenda();
+    }
+
+    APlusAgenda() {
+        System.out.println("Testing testing!!");
     }
 }
